f336ccf5d1f97b89fb087a8152ef8a2c0277bfbd
FauzanEriandi <146106979+FauzanEriandi@users.noreply.github.com>
2024-02-27 21:20:42 +0700

    Add files via upload

---
+++ arrayData.php
@@ -0,0 +1,25 @@
+<h3>Daftar Nilai Siswa</h3>
+<table border="1" width="100%">
+<thead>
+    <tr>
+        <th>No</th><th>NIM</th><th>UTS</th>
+        <th>UAS</th><th>Tugas</th><th>Nilai Akhir</th>
+    </tr>
+</thead>
+    <tbody>
+        <?php
+        $nomor = 1;
+        foreach ($nilai as $n) {
+            echo '<tr><td>'.$nomor.'</td>';
+            echo '<td>'.$ns['nim'].'</td>';
+            echo '<td>'.$ns['uts'].'</td>';
+            echo '<td>'.$ns['uas'].'</td>';
+            echo '<td>'.$ns['tugas'].'</td>';
+            $nilai_akhir = ($ns['uts'] + $ns['uas']+$ns['tugas'])/3;
+            echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
+            echo '<tr/>';
+            $nomor++;
+            }
+            ?>
+    </tbody>
+</table>
